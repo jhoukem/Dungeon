1c51b36b4faa93d73658030a66cd154822b115bd
unknown <jhoukem@gmail.com>
2015-09-15 22:19:24 +0200

    refactoring/ travail sur EnigmRoom

---
+++ src/model/RoomFactory.java
@@ -0,0 +1,24 @@
+package model;
+
+import java.util.ArrayList;
+
+public class RoomFactory {
+
+
+	public static Room generateRoom(String s, ArrayList<Room> rooms){
+		Room r = null;
+
+		if(s.equals("e"))
+			r = new EnigmRoom(rooms.size() + 1);
+		else if(s.equals("r"))
+			r = new Room(rooms.size() + 1);
+		else if(s.equals("m"))
+			r = new MonsterRoom(rooms.size() + 1);
+		else
+			System.out.println("TYPE INCONNU !");
+		rooms.add(r);
+
+		return r;
+	}
+
+}
