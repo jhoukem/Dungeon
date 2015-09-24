5f6d41e613a8139c0f6207afebef002e45dc614f
jhoukem <jhouke@gmail.com>
2015-09-24 17:47:53 +0200

    harmonisation fichiers txt

--- src/test/Test.java
+++ src/test/Test.java
@@ -7,17 +7,16 @@ import java.io.File;
 import java.util.ArrayList;
 
 import exceptions.UnknowRoomTypeException;
+import items.Key;
+import model.Direction;
+import model.Dungeon;
+import model.GenerateFromFile;
+import model.Question;
 import rooms.EnigmaRoom;
 import rooms.MonsterRoom;
 import rooms.Room;
 import rooms.RoomFactory;
 import rooms.TrapRoom;
-import items.Key;
-import model.Direction;
-import model.GenerateFromFile;
-import model.Dungeon;
-import model.FileParser;
-import model.Question;
 
 public class Test {
 
@@ -139,6 +138,12 @@ public class Test {
 			isMonster = true;
 		assertTrue(isMonster);
 		
+		r2 = RoomFactory.generateRoom("Glouton", new ArrayList<Room>());
+		isMonster = false;
+		if(r2 instanceof MonsterRoom)
+			isMonster = true;
+		assertTrue(isMonster);
+		
 		Room r3 = RoomFactory.generateRoom("Exit", new ArrayList<Room>());
 		boolean isExit = false;
 		if(r3 instanceof Room && r3.isExit())
@@ -159,60 +164,60 @@ public class Test {
 		
 	}
 
-	//	@org.junit.Test
-	//	public void testParseFile() {
-	//		Dungeon dj = new Dungeon();
-	//		dj.setRooms(DjGeneratorFromFile.generateDjFromFile(new File("testDj.txt")));
-	//		//Room1
-	//		Room r1 = dj.getRooms().get(0);
-	//		assertTrue(r1.hasNorthSide);
-	//		assertTrue(r1.hasWestSide);
-	//		assertFalse(r1.hasEastSide);
-	//		assertFalse(r1.hasSouthSide);
-	//
-	//		//Room2
-	//		Room r2 = dj.getRooms().get(1);
-	//		assertTrue(r2.hasNorthSide);
-	//		assertTrue(r2.hasSouthSide);
-	//		assertFalse(r2.hasEastSide);
-	//		assertFalse(r2.hasWestSide);
-	//
-	//		//Room3
-	//		Room r3 = dj.getRooms().get(2);
-	//		assertTrue(r3.hasNorthSide);
-	//		assertTrue(r3.hasEastSide);
-	//		assertFalse(r3.hasWestSide);
-	//		assertFalse(r3.hasSouthSide);
-	//
-	//		//Room4
-	//		Room r4 = dj.getRooms().get(3);
-	//		assertTrue(r4.hasNorthSide);
-	//		assertTrue(r4.hasWestSide);
-	//		assertTrue(r4.hasSouthSide);
-	//		assertFalse(r4.hasEastSide);
-	//
-	//		//Room5
-	//		Room r5 = dj.getRooms().get(4);
-	//		assertTrue(r5.hasSouthSide);
-	//		assertFalse(r5.hasWestSide);
-	//		assertFalse(r5.hasEastSide);
-	//		assertFalse(r5.hasNorthSide);
-	//
-	//		//Room6
-	//		Room r6 = dj.getRooms().get(5);
-	//		assertTrue(r6.hasEastSide);
-	//		assertFalse(r6.hasWestSide);
-	//		assertFalse(r6.hasNorthSide);
-	//		assertFalse(r6.hasSouthSide);
-	//
-	//		//Room7
-	//		Room r7 = dj.getRooms().get(6);
-	//		assertTrue(r7.hasSouthSide);
-	//		assertFalse(r7.hasWestSide);
-	//		assertFalse(r7.hasEastSide);
-	//		assertFalse(r7.hasNorthSide);
-	//
-	//	}
+		@org.junit.Test
+		public void testParseFile() {
+			Dungeon dj = new Dungeon();
+			dj.setRooms(GenerateFromFile.generateDjFromFile(new File("testDj.txt")));
+			//Room1
+			Room r1 = dj.getRooms().get(0);
+			assertTrue(r1.hasNorthSide);
+			assertTrue(r1.hasWestSide);
+			assertFalse(r1.hasEastSide);
+			assertFalse(r1.hasSouthSide);
+	
+			//Room2
+			Room r2 = dj.getRooms().get(1);
+			assertTrue(r2.hasNorthSide);
+			assertTrue(r2.hasSouthSide);
+			assertFalse(r2.hasEastSide);
+			assertFalse(r2.hasWestSide);
+	
+			//Room3
+			Room r3 = dj.getRooms().get(2);
+			assertTrue(r3.hasNorthSide);
+			assertTrue(r3.hasEastSide);
+			assertFalse(r3.hasWestSide);
+			assertFalse(r3.hasSouthSide);
+	
+			//Room4
+			Room r4 = dj.getRooms().get(3);
+			assertTrue(r4.hasNorthSide);
+			assertTrue(r4.hasWestSide);
+			assertTrue(r4.hasSouthSide);
+			assertFalse(r4.hasEastSide);
+	
+			//Room5
+			Room r5 = dj.getRooms().get(4);
+			assertTrue(r5.hasSouthSide);
+			assertFalse(r5.hasWestSide);
+			assertFalse(r5.hasEastSide);
+			assertFalse(r5.hasNorthSide);
+	
+			//Room6
+			Room r6 = dj.getRooms().get(5);
+			assertTrue(r6.hasEastSide);
+			assertFalse(r6.hasWestSide);
+			assertFalse(r6.hasNorthSide);
+			assertFalse(r6.hasSouthSide);
+	
+			//Room7
+			Room r7 = dj.getRooms().get(6);
+			assertTrue(r7.hasSouthSide);
+			assertFalse(r7.hasWestSide);
+			assertFalse(r7.hasEastSide);
+			assertFalse(r7.hasNorthSide);
+	
+		}
 
 	@org.junit.Test
 	public void testQuestion() {
